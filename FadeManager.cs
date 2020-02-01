b552ca54176c92eb4aea131b281ba6a09c0a75e1
StoneKnight117 <60109362+StoneKnight117@users.noreply.github.com>
2020-02-01 13:37:16 -0500

    Interaction Scripts and Fade effect

---
+++ FadeManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeManager : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeSpeed;
+    public bool fading;
+
+    private void Awake() {
+        fadeImage.transform.localScale = new Vector2(Screen.width, Screen.height);
+        StartCoroutine(Fade());
+    }
+
+    public IEnumerator Fade() {
+        fading = true;
+        yield return FadeIn();
+        yield return new WaitForSeconds(0.5f);
+        yield return FadeOut();
+        fading = false;
+    }
+
+    public IEnumerator FadeIn() {
+        while(fading) {
+            fadeImage.color = Color.Lerp(fadeImage.color, Color.black, fadeSpeed * Time.deltaTime);
+            if (fadeImage.color.a >= 0.95f) {
+                fadeImage.color = Color.black;
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeOut() {
+        while (fading) {
+            fadeImage.color = Color.Lerp(fadeImage.color, Color.clear, fadeSpeed * Time.deltaTime);
+            if (fadeImage.color.a <= 0.05f) {
+                fadeImage.color = Color.clear;
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public string tool;
+    public Collider2D collider;
+    public List<Collider2D> interactables;
+    public ContactFilter2D contactFilter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D)) {
+            this.transform.position = this.transform.position + this.transform.right * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A)) {
+            this.transform.position = this.transform.position - this.transform.right * speed * Time.deltaTime;
+        }
+
+        collider.OverlapCollider(contactFilter, interactables);
+
+        if(interactables.Count >= 1) {
+            if(Input.GetKeyDown(KeyCode.E)) {
+                interactables[0].gameObject.GetComponent<Interactables>().Interact(tool);
+            }
+        }
+    }
+}
